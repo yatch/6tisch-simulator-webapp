3b9a1a74e46eaa4e072f6da850da7f3f1ca0c876
Yasuyuki Tanaka <yasuyuki.tanaka@inria.fr>
2019-01-07 18:36:15 +0100

    Show the connection status of the backend server

--- src/components/ToolBar.vue
+++ src/components/ToolBar.vue
@@ -4,6 +4,13 @@
     <span>6TiSCH</span>
     <span class="font-weight-light">Simulator</span>
   </v-toolbar-title>
+  <v-tooltip bottom>
+    <v-chip small light slot="activator">
+      <span>status</span>
+      <v-icon :color="color">{{ icon }}</v-icon>
+    </v-chip>
+    <span>{{ connection }}</span>
+  </v-tooltip>
   <v-spacer></v-spacer>
   <v-toolbar-side-icon @click.stop="navigationDrawer = !navigationDrawer"></v-toolbar-side-icon>
 </v-toolbar>
@@ -16,6 +23,21 @@ export default {
       get () { return this.$store.state.navigationDrawer },
       set (newValue) {
         return this.$store.dispatch('changeNavigationDrawerState', newValue)
+      }
+    },
+    connection () { return this.$store.state.connection },
+    color () {
+      if (this.connection === 'connected') {
+        return 'success'
+      } else {
+        return 'error'
+      }
+    },
+    icon () {
+      if (this.connection === 'connected') {
+        return 'check'
+      } else {
+        return 'error'
       }
     }
   }
