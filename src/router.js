9e45456aedc0e876f715bc7b1895c4920dae4e53
Yasuyuki Tanaka <yasuyuki.tanaka@inria.fr>
2019-01-10 18:47:30 +0100

    frontend: add Dashboard view

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Dashboard from '@/views/Dashboard'
 
 Vue.use(Router)
 
@@ -9,7 +10,8 @@ export default new Router({
   routes: [
     {
       name: 'Dashboard',
-      path: '/'
+      path: '/',
+      component: Dashboard
     }
   ]
 })
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,19 @@
+<template>
+<v-flex xs12>
+  <v-container grid-list-sm>
+    <v-layout
+      row
+      wrap
+      >
+    </v-layout>
+  </v-container>
+</v-flex>
+</template>
+
+<script>
+export default {
+  components: {
+
+  }
+}
+</script>
