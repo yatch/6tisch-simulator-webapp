f35c7766a550a0893dbe753fc4850cf2855af236
Yasuyuki Tanaka <yasuyuki.tanaka@inria.fr>
2019-01-10 14:30:47 +0100

    frontend: remove components and views

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <template>
   <v-app>
-    <ToolBar/>
-    <NavigationDrawer/>
-    <AutoReload/>
     <v-content>
       <router-view/>
     </v-content>
@@ -10,16 +7,9 @@
 </template>
 
 <script>
-import ToolBar from './components/ToolBar'
-import NavigationDrawer from './components/NavigationDrawer'
-import AutoReload from './components/AutoReload'
-
 export default {
   name: 'App',
   components: {
-    ToolBar,
-    NavigationDrawer,
-    AutoReload
   },
   data () {
     return {
@@ -27,35 +17,6 @@ export default {
     }
   },
   created () {
-    document.addEventListener('DOMContentLoaded', () => {
-      // window.eel is not initialized until DOMContentLoaded is
-      // fired. checking window.eel in an DOMContentLoaded event
-      // handler may work better than doing that in created() directly
-      if (window.eel === undefined) {
-        this.$store.dispatch('simulator/disconnect', 'disconnected')
-      } else {
-        this.$store.dispatch('simulator/connect', 'connected')
-        window.eel._websocket.addEventListener(
-          'close',
-          () => {
-            this.$store.dispatch('simulator/disconnect', 'disconnected')
-          }
-        )
-        window.eel.get_default_settings()(defaultSettings => {
-          this.$store.dispatch('simulator/saveSettings', defaultSettings)
-        })
-        window.eel.get_available_scheduling_functions()(availableSFs => {
-          this.$store.dispatch('simulator/setAvailableSFs', availableSFs)
-        })
-        window.eel.get_available_connectivities()(availableConnectivities => {
-          this.$store.dispatch('simulator/setAvailableConnectivities',
-                               availableConnectivities)
-        })
-        window.eel.get_git_info()(gitInfo => {
-          this.$store.dispatch('simulator/setGitInfo', gitInfo)
-        })
-      }
-    })
   }
 }
 </script>
--- src/router.js
+++ src/router.js
@@ -1,29 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import DashBoard from '@/views/DashBoard.vue'
-import Configuration from '@/views/Configuration.vue'
-import Results from '@/views/Results.vue'
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: [
-    {
-      name: 'dashboard',
-      path: '/',
-      component: DashBoard
-    },
-    {
-      name: 'config',
-      path: '/config',
-      component: Configuration
-    },
-    {
-      name: 'results',
-      path: '/results',
-      component: Results
-    }
-  ]
+  routes: []
 })
