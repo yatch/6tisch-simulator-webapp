63a936e7fe4cf61ba5b55af3539ac6c5fcb7368f
Yasuyuki Tanaka <yasuyuki.tanaka@inria.fr>
2019-01-10 22:08:39 +0100

    frontend: add BaseKPICard

---
+++ src/components/BaseKPICard.vue
@@ -0,0 +1,27 @@
+<template>
+<v-flex
+  xs12
+  md4
+  >
+  <v-tooltip bottom>
+    <v-card slot="activator" height="100%">
+      <v-card-title class="subheading">
+        <slot name="title"/>
+      </v-card-title>
+      <v-card-text class="title">
+        <v-layout justify-center>
+          <slot name="main-contents"/>
+        </v-layout>
+      </v-card-text>
+      <v-card-text>
+        <v-layout justify-center>
+          <slot name="sub-contents"/>
+        </v-layout>
+      </v-card-text>
+    </v-card>
+    <span>
+      <slot name="help"/>
+    </span>
+  </v-tooltip>
+</v-flex>
+</template>
