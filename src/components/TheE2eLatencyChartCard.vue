64b2a45268389325372e29696b7c4ee2d529a420
Yasuyuki Tanaka <yasuyuki.tanaka@inria.fr>
2019-01-11 02:42:32 +0100

    frontend: add TheE2eLatencyChartCard

---
+++ src/components/TheE2eLatencyChartCard.vue
@@ -0,0 +1,115 @@
+<template>
+<BaseChartCard
+  type="line"
+  :options="options"
+  :series="series"
+  >
+  <span slot="help">
+    E2E PDR trend in the last 10 minutes. Each sample is the E2E PDR
+    value within a given minute window.
+  </span>
+</BaseChartCard>
+</template>
+
+<script>
+import Simulation from '@/mixins/Simulation'
+import Simulator from '@/mixins/Simulator'
+import BaseChartCard from '@/components/BaseChartCard'
+
+export default {
+  components: {
+    BaseChartCard
+  },
+  mixins: [Simulator, Simulation],
+  data () {
+    return {
+      options: {
+        chart: {
+          id: 'latency',
+          group: 'sync-charts',
+          toolbar: { show: false },
+          animations: { enabled: false }
+        },
+        legend: { showForSingleSeries: true },
+        xaxis: { labels: { show: false } },
+        yaxis: {
+          tickAmount: 6,
+          decimalsInFloat: 0
+        }
+      },
+      series: [
+        {
+          name: 'Avg E2E Lat (s)',
+          data: []
+        },
+        {
+          name: 'Max E2E Lat (s)',
+          data: []
+        }
+      ],
+      numRecords: 0,
+      latencySum: 0,
+      latencyMax: 0
+    }
+  },
+  computed: {
+    latencyAvg () {
+      if (this.numRecords === 0) {
+        return 0
+      } else {
+        return this.latencySum / this.numRecords
+      }
+    }
+  },
+  watch: {
+    $_simulator_operationalStatus (newStatus, oldStatus) {
+      if ((newStatus === 'running' && oldStatus === 'ready') ||
+          (newStatus === 'aborted')) {
+        this.reset()
+      }
+    },
+    $_simulation_elapsedMinutes (newValue) {
+      if (newValue !== null) {
+        // update Avg
+        this.series[0].data.push({
+          x: newValue,
+          y: this.latencyAvg
+        })
+        this.series[0].data = this.series[0].data.slice(-10)
+
+        // update Max
+        this.series[1].data.push({
+          x: newValue,
+          y: this.latencyMax
+        })
+        this.series[1].data = this.series[1].data.slice(-10)
+
+        // reset the counters
+        this.numRecords = 0
+        this.latencySum = 0
+        this.latencyMax = 0
+      }
+    },
+    $_simulation_lastlastAppPacketEvent (event) {
+      if (event !== null && event.type === 'rx') {
+        const latency = ((event.asn - event.packet.app.timestamp) *
+                         this.$_simulator_settings.tsch_slotDuration)
+        this.numRecords += 1
+        this.latencySum += latency
+        if (this.latencyMax === undefined || this.latencyMax < latency) {
+          this.latencyMax = latency
+        }
+      }
+    }
+  },
+  methods: {
+    reset () {
+      this.series[0].data = []
+      this.series[1].data = []
+      this.numRecords = 0
+      this.latencySum = 0
+      this.latencyMax = 0
+    }
+  }
+}
+</script>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -14,6 +14,7 @@
       wrap
       >
       <TheE2ePdrChartCard/>
+      <TheE2eLatencyChartCard/>
     </v-layout>
   </v-container>
 </v-flex>
@@ -24,13 +25,15 @@ import TheOverallE2ePdrCard from '@/components/TheOverallE2ePdrCard'
 import TheOverallE2eLatencyCard from '@/components/TheOverallE2eLatencyCard'
 import TheSlotframeOccupancyCard from '@/components/TheSlotframeOccupancyCard'
 import TheE2ePdrChartCard from '@/components/TheE2ePdrChartCard'
+import TheE2eLatencyChartCard from '@/components/TheE2eLatencyChartCard'
 
 export default {
   components: {
     TheOverallE2ePdrCard,
     TheOverallE2eLatencyCard,
     TheSlotframeOccupancyCard,
-    TheE2ePdrChartCard
+    TheE2ePdrChartCard,
+    TheE2eLatencyChartCard
   }
 }
 </script>
