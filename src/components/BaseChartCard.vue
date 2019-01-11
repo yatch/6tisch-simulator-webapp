24c5a36151116e4b69293e2610e22510d30477a9
Yasuyuki Tanaka <yasuyuki.tanaka@inria.fr>
2019-01-11 01:56:40 +0100

    frontend: add BaseChartCard

---
+++ src/components/BaseChartCard.vue
@@ -0,0 +1,25 @@
+<template>
+<v-flex
+  xs12
+  md4
+  >
+  <v-tooltip bottom>
+    <v-card slot="activator" height="100%">
+      <apexchart
+        :type="type"
+        height="200px"
+        :options="options"
+        :series="series"/>
+    </v-card>
+    <span>
+      <slot name="help"/>
+    </span>
+  </v-tooltip>
+</v-flex>
+</template>
+
+<script>
+export default {
+  props: ['type', 'options', 'series']
+}
+</script>
