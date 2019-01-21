9c453e188964736a565e6fcd92fd5b754d8dfa2f
Yasuyuki Tanaka <yasuyuki.tanaka@inria.fr>
2019-01-21 14:22:04 +0100

    frontend: adjust padding

--- src/components/BaseKPICard.vue
+++ src/components/BaseKPICard.vue
@@ -5,7 +5,7 @@
   >
   <v-tooltip bottom>
     <v-card slot="activator" height="100%">
-      <v-card-title class="body-2">
+      <v-card-title class="body-2 pb-0">
         <slot name="title"/>
       </v-card-title>
       <v-card-text class="title">
